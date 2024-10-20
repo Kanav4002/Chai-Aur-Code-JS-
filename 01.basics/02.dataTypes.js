b09fddd75a3fd43129c452e9eb7a0753c6c00f39
Kanav4002 <kumarkanav5753@gmail.com>
2024-10-20 16:37:50 +0530

    learned datatypes

---
+++ 01.basics/02.dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treal all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 3)
+console.log("Kanav")
+
+
+let name = "Kanav"
+let age = 20
+let isLoggedIn = true
+let state; 
+
+// Primitive Data Types
+// number => 2 to power 53 - 1
+// bigint => 2 to power 53 - 1
+// string => ""
+// boolean => true/false
+// null => standalone value // represents for an empty value
+// undefined => value is not assigned
+// symbol => unique
+
+// object
+console.log(typeof undefined);  // undefined
+console.log(typeof null); // object
+
