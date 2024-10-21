8c66e08bbf727002728b37a97df309059bfa494a
Kanav4002 <kumarkanav5753@gmail.com>
2024-10-21 11:50:20 +0530

    nums and math

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,41 @@
+const score = 400
+// console.log(score)
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(typeof balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.8966
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// Number.MAX_VALUE
+// Number.MIN_VALUE
+// Number.MAX_SAFE_INTEGER
+// Number.MIN_SAFE_INTEGER
+
+// +++++++++++++++++++++++++++++++ Maths +++++++++++++++++++++++++++++++++++++
+
+// console.log(Math)
+// console.log(Math.abs(-100));
+// console.log(Math.round(1.6));
+// console.log(Math.ceil(1.6));
+// console.log(Math.min(4, 6, 7, 9));
+// console.log(Math.max(4, 6, 7, 9));
+
+// console.log(Math.random());
+// console.log((Math.random() * 10) + 1);
+// console.log(Math.floor((Math.random() * 10)) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
+
