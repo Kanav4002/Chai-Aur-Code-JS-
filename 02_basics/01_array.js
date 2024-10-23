34991c63568b248d1cfaf4528816360663b16699
Kanav4002 <kumarkanav5753@gmail.com>
2024-10-23 13:55:09 +0530

    array basics

---
+++ 02_basics/01_array.js
@@ -0,0 +1,42 @@
+// array
+// JS arrays are resizable.
+// JS are not associative arrays.
+// JS arrays are zero-indexed.
+// JS arrays copy operations create shallow copies : a shallow copy of an object where properties share the same reference.
+const myArr = [1, 2, 3, 4, 5];
+const myHeroes = ["Spiderman", "Ironman"];
+
+const myArr2 =  new Array(1, 2, 3, 4);
+
+// console.log(myArr);
+// console.log(myArr[1]);
+
+
+// Array methods
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(9));
+
+// const newArr = myArr.join();
+
+console.log(myArr);
+// console.log(newArr);
+
+
+// slice, splice
+// console.log("A ", myArr);
+
+const myn1 = myArr.slice(1, 3);
+console.log(myn1);
+console.log("B ", myArr);
+
+const myn2 = myArr.slice(1, 3);
+console.log(myn2);
+console.log("C ", myArr);
