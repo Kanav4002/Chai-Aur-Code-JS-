88355257054b57a59cb53d0bdae48259ee59f2db
Kanav4002 <kumarkanav5753@gmail.com>
2025-02-17 23:57:34 +0530

    did while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,20 @@
+// while loop
+
+let index = 0;
+// while (index <= 10) {
+//   console.log("Index is: " + index);
+//   index = index + 2;
+// }
+
+let myArray = ["Batman", "Superman", "Flash", "Wonder Woman"];
+// let i = 0;
+// while (i < myArray.length) {
+//   console.log(myArray[i]);
+//   i++;
+// }
+
+let x = 10;
+do {
+  console.log("This will run at least once");
+  x++;
+} while(x < 10);
